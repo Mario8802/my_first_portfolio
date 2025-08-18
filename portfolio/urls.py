03af5977e7c9d3e6bfe9ef7c0fcecb74d25e9e6b
Mario <144950133+Mario8802@users.noreply.github.com>
2025-08-18 16:20:34 +0200

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,13 +3,19 @@ from django.urls import path, include
 from portfolio import views
 from certificates.views import certificate_list
 from django.conf.urls.i18n import i18n_patterns
+from django.conf import settings
+from django.http import Http404
+
+def fake_admin_404(request, *args, **kwargs):
+    raise Http404()
 
 urlpatterns = [
     path("i18n/", include("django.conf.urls.i18n")),
 ]
 
 urlpatterns += i18n_patterns(
-    path("admin/", admin.site.urls),
+    path(settings.ADMIN_URL, admin.site.urls),
+    path("admin/", fake_admin_404, name="fake-admin"),
     path("", views.home, name="home"),
     path("projects/", include("projects.urls")),
     path("contact/", include("contact.urls")),
